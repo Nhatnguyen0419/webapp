9b6085d5ff5fba9e50c01ecc5ae8c682a925eef2
fncng193 <nhatnguyen0419@gmail.com>
2020-06-30 13:56:20 +0700

    Fluent Validation

--- eShopSolution/eShopSolution.BackendApi/Startup.cs
+++ eShopSolution/eShopSolution.BackendApi/Startup.cs
@@ -7,6 +7,9 @@ using eShopSolution.Application.System.Users;
 using eShopSolution.Data.EF;
 using eShopSolution.Data.Entities;
 using eShopSolution.Utilities.Constants;
+using eShopSolution.ViewModels.System.Users;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +31,7 @@ namespace eShopSolution.BackendApi
         }
 
         public IConfiguration Configuration { get; }
-        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -39,20 +42,22 @@ namespace eShopSolution.BackendApi
                 .AddEntityFrameworkStores<EShopDbContext>()
                 .AddDefaultTokenProviders();
             //Declare DI
-            services.AddTransient<IPublicProductService,PublicProductService>();
-            services.AddTransient<IManageProductService,ManageProductService>();
-            services.AddTransient<IStorageService,FileStorageService>();
-            services.AddTransient<UserManager<AppUser>,UserManager<AppUser>>();
+            services.AddTransient<IPublicProductService, PublicProductService>();
+            services.AddTransient<IManageProductService, ManageProductService>();
+            services.AddTransient<IStorageService, FileStorageService>();
+            services.AddTransient<UserManager<AppUser>, UserManager<AppUser>>();
             services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
             services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
             services.AddTransient<IUsersService, UsersService>();
-            
-            
-            services.AddControllers();
+
+            //services.AddTransient<IValidator<LoginRequest>, LoginRequestValidator>();
+            //services.AddTransient<IValidator<RegisterRequest>, RegisterRequestValidator>();
+
+            services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<LoginRequestValidator>());
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1",new OpenApiInfo{ Title ="Swagger eShopSolution,", Version="v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger eShopSolution,", Version = "v1" });
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
                     Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
